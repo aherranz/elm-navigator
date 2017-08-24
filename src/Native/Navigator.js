15648ea9ded4548c53515010eef8a1ef21847cb1
Ángel Herranz <aherranz@fi.upm.es>
2017-08-24 15:33:06 +0200

    Bug fixed in native implementation (window.userAgent) and conventions to set source files.

--- src/Native/Navigator.js
+++ src/Native/Navigator.js
@@ -1,6 +1,7 @@
 
 var _aherranz$elm_navigator$Native_Navigator = function(elm) {
 
-    let userAgent = window && window.navigator && window.userAgent;
+    let userAgent = window && window.navigator && window.navigator.userAgent;
+
     return  { userAgent: userAgent || "" };
 }();
