44a2667d56d1dcf50918a34ef56eb9dbe2723a0b
Ángel Herranz <aherranz@fi.upm.es>
2017-10-20 17:13:11 +0200

    Navigator.elm formatted with elm-format.

--- src/Navigator.elm
+++ src/Navigator.elm
@@ -1,15 +1,18 @@
 module Navigator exposing (userAgent)
 
 {-| A module that exposes the read-only properties of `window.navigator`
-    ([Navigator](https://developer.mozilla.org/en/docs/Web/API/Navigator)):
-    the state and the identity of the user agent
+([Navigator](https://developer.mozilla.org/en/docs/Web/API/Navigator)):
+the state and the identity of the user agent
 
 @docs userAgent
+
 -}
 
 import Native.Navigator
 
+
 {-| Returns the userAgent
 -}
 userAgent : String
-userAgent = Native.Navigator.userAgent
+userAgent =
+    Native.Navigator.userAgent
